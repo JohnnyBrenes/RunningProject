7d464915d8f0a129909a15de256809db88249b02
JohnnyBrenes <johnnybrenes@hotmail.com>
2025-03-26 09:13:29 -0600

    Test

---
+++ Backend/RunningWebApi/Models/TrainningResponseDto.cs
@@ -0,0 +1,12 @@
+namespace RunningWebApi.Models;
+
+public class TrainningResponseDto
+{
+    public Guid Id { get; set; }
+    public DateTime Date { get; set; }
+    public double Kilometers { get; set; }
+    public string Time { get; set; } = string.Empty;
+    public string Pace { get; set; } = string.Empty;
+    public string Shoes { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+}
